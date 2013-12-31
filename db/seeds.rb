ab59b2035eef144c3db691410f62f05c00952d9c
yafeilee <lyfi2003@gmail.com>
2013-12-31 22:52:02 +0800

    seeds user confirm! -> save

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,5 +15,5 @@ end
 puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
-#user.confirm!
+user.save!(:validate => false)
 user.add_role :admin
