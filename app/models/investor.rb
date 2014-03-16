4591d27d1f6a33b104357085d5123228ccf5d786
yafeilee <lyfi2003@gmail.com>
2014-03-16 12:37:31 +0800

    i18n 支持

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -1,5 +1,15 @@
 class Investor < ActiveRecord::Base
-  INVESTOR_TYPE = [ "个人投资", "机构投资" ]
+  #INVESTOR_TYPE = [ "个人投资", "机构投资" ]
+  INVESTOR_TYPE = []
+  [
+    :PERSON,
+    :ORGANIZATION,
+  ].each do |e|
+    real_name = e.to_s.underscore
+    INVESTOR_TYPE << real_name
+    const_set(e, real_name)
+  end
+
   belongs_to :user
   has_many :investment
   has_one :investidea
