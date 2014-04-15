41a250c84cf6b63c9ccfd527bfaed7bd693a5552
yafeilee <lyfi2003@gmail.com>
2014-04-15 14:27:26 +0800

    添加最小高度限制

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
+  #before_filter :authenticate_user!
 
   def index
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
