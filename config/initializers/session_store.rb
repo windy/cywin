669032d24958d05e14f15589db2ab556ec7def05
yafeilee <lyfi2003@gmail.com>
2014-04-21 11:05:20 +0800

    升级 rails 至 4.1.0

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-Eachfund::Application.config.session_store :cookie_store, key: '_eachfund_session'
+Rails.application.config.session_store :cookie_store, key: '_php_session'
