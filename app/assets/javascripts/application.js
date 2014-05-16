fc8ab84921cf4993893e87ed60d7fc647b130a0e
yafeilee <lyfi2003@gmail.com>
2014-05-16 14:57:14 +0800

    left tree can't show bug fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui-1.10.4.custom
-//= require foundation
 //= require angularjs
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+//$(function(){ $(document).foundation(); });
