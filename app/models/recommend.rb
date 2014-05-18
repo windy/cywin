51171595d2f43148a7f8b610bb6c74e62b195b5e
yafeilee <lyfi2003@gmail.com>
2014-05-18 09:45:23 +0800

    精品管理: project_id 不存在的处理

--- app/models/recommend.rb
+++ app/models/recommend.rb
@@ -3,7 +3,9 @@ class Recommend < ActiveRecord::Base
 
   validates_presence_of :project_id
   validates_uniqueness_of :project_id
-  validates_associated :project
+  validate do |r|
+    errors.add(:project_id, "ID 不存在") unless Project.where(id: r.project_id).first
+  end
 
   default_scope -> { where(deleted: false) }
 end
