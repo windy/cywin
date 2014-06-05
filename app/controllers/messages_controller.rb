fd65400085cadb3bf3a0d431c06809dfc0263162
yafeilee <lyfi2003@gmail.com>
2014-06-05 18:31:29 +0800

    fix 消息处理

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,8 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
   def index
     @messages = current_user.messages.default_order.page(params[:page])
-    @count = @messages.size
-    @waiting_count = @messages.untreat.size
+    @all = current_user.messages
+    @count = @all.size
+    @waiting_count = @all.untreat.size
   end
 end
