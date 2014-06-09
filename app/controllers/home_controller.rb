b4ad4c3540ce3bb4e9cdaf00a399324062112e52
yafeilee <lyfi2003@gmail.com>
2014-06-09 10:11:35 +0800

    修正空时间线报错的问题

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if current_user
       @events = Event.default_order.limit(Event::PER_PAGE).related(current_user.id)
-      @start_with = @events[-1].created_at.to_i
+      @start_with = @events[-1].try(:created_at).to_i
       render 'login'
     else
       render 'nologin'
