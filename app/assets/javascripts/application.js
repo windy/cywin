588d00d11088f831afd5f2bb73aaaa44cb65d3ce
yafeilee <lyfi2003@gmail.com>
2014-06-18 18:16:27 +0800

    搜索框的 placeholder 调整

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,7 @@
 //= require jquery_ujs
 //= require angularjs
 //= require foundation/foundation
+//= require foundation/foundation.topbar
 //= require foundation/foundation.orbit
 //= require foundation/foundation.alert
 //= require_tree .
