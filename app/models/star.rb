19c315fce1609b5547f0d8c6b074202070a3281f
yafeilee <lyfi2003@gmail.com>
2014-06-27 14:23:04 +0800

    趋势页面的功能开发

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,4 +1,8 @@
 class Star < ActiveRecord::Base
+
   belongs_to :user
   belongs_to :project
+
+  scope :most_a_week, ->(t=6) { group(:project_id).order("COUNT(*) DESC").having("created_at > ?", 1.week.ago).limit(t).count }
+
 end
--- app/models/talk.rb
+++ app/models/talk.rb
@@ -3,4 +3,6 @@ class Talk < ActiveRecord::Base
   belongs_to :user
   belongs_to :target, polymorphic: true
 
+  scope :most_a_week, ->(t=6) { group(:target_id).order("COUNT(*) DESC").having("created_at > ? AND target_type = 'Project'", 1.week.ago).limit(t).count }
+
 end
