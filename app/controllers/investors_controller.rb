3485d246dae63b376be245096a8fddc7243a7b98
yafeilee <lyfi2003@gmail.com>
2014-08-04 23:41:11 +0800

    首页的内容, 数据还不是真实的

--- app/controllers/investors_controller.rb
+++ app/controllers/investors_controller.rb
@@ -1,5 +1,5 @@
 class InvestorsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @investors = Investor.passed.default_order.page(params[:page])
