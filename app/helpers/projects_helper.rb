522b09e7fa1a34b2210a09b8a88aed614e4e5539
yafeilee <lyfi2003@gmail.com>
2014-08-16 21:21:50 +0800

    链接分类

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,8 @@
 module ProjectsHelper
+
+  def project_categories_link(project)
+    project.categories.collect do |category|
+      link_to category.name, search_explore_index_path(head_id: category.head_id)
+    end.join(', ')
+  end
 end
