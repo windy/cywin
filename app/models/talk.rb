98a46e4a76393edf2fe2fb90c223e2ca41cc9b64
yafeilee <lyfi2003@gmail.com>
2014-08-21 01:46:56 +0800

    重构发现页面

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -3,6 +3,8 @@ class Talk < ActiveRecord::Base
   belongs_to :user
   belongs_to :target, polymorphic: true
 
-  scope :most_a_week, ->(t=6) { group(:target_id).order("COUNT(*) DESC").having("'created_at' > ? AND 'target_type' = 'Project'", 1.week.ago).limit(t).count }
+  scope :most_a_week, ->(t=6) { group(:target_id).order("COUNT(*) DESC").having("created_at > ? AND target_type = 'Project'", 1.week.ago).limit(t).count }
+  
+  scope :most_a_month, ->(t=6) { group(:target_id).order("COUNT(*) DESC").having("created_at > ? AND target_type = 'Project'", 1.month.ago).limit(t).count }
 
 end
