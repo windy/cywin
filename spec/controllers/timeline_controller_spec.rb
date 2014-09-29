9134a0d7fe7740379358a4cf06a5697b73bb7579
yafeilee <lyfi2003@gmail.com>
2014-09-29 16:49:59 +0800

    timeline css

--- spec/controllers/timeline_controller_spec.rb
+++ spec/controllers/timeline_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe TimelineController do
 
   describe "GET 'index'" do
+    login_user
     it "returns http success" do
       get 'index'
       response.should be_success
